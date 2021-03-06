ea452bcbad66793982dc40f2e14be0d6415a9cb5
Catarina <c.alexandracorreia@ua.pt>
2021-03-06 20:12:20 +0000

    Minor change

--- mock_sensors/motion_smoke/mock_motion_smoke.py
+++ mock_sensors/motion_smoke/mock_motion_smoke.py
@@ -47,7 +47,6 @@ def on_connect(mqttc, obj, flags, rc):
             msg = json.dumps({
                 'name': f"{t}_{i+1}",
                 'availability_topic': f'{CLIENT_ID}/status',  # Online, Offline
-                'device_class': 'None',
                 'unit_of_measurement': '' if t == 'motion' else '',
                 'unique_id': f"{t}_{i+1}",
                 'state_topic': f'{t}_{i+1}/state',  # Value published
